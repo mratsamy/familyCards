dc4a01e5058461bda156dd3a459747349c75cea8
Michael Ratsamy <michael.ratsamy@gmail.com>
2018-06-29 01:07:53 -0700

    Updated loken logic.

--- client_src/src/App.js
+++ client_src/src/App.js
@@ -55,7 +55,7 @@ class App extends Component {
 
 const mapStateToProps = state => ({
   token: state.token,
-  isAdmin: state.isAdmin
+  isAdmin: state.user.isAdmin
 })
 
 export default withRouter(
